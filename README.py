86d116b69a944d91ea036bbdf00d50d3d3f5eb77
Andrei Solot <95416215+Andrei-Solot@users.noreply.github.com>
2023-01-02 15:57:51 +0000

    Update README.py

--- README.py
+++ README.py
@@ -1,9 +1,9 @@
 def generate_short_readme(name, specialization):
     readme = f"# {name} - Machine Learning Engineer (Specialization in {specialization})\n\n"
-    readme += "[Insert a brief introduction and overview of your skills and experience here]"
+    readme += "I am a Masters graduate from Goldsmiths University of London. I am a competent and committed person looking for a rewarding career in computational neuroscience and AI engineering."
     return readme
 
-name = "Your Name"
+name = "Andrei Solot"
 specialization = "Neuroscience"
 
 print(generate_short_readme(name, specialization))
