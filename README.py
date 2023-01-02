ecb81fda5df06d0aae7bcbdb5839f0ee9b25af4d
Andrei Solot <95416215+Andrei-Solot@users.noreply.github.com>
2023-01-02 15:56:57 +0000

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,9 @@
+def generate_short_readme(name, specialization):
+    readme = f"# {name} - Machine Learning Engineer (Specialization in {specialization})\n\n"
+    readme += "[Insert a brief introduction and overview of your skills and experience here]"
+    return readme
+
+name = "Your Name"
+specialization = "Neuroscience"
+
+print(generate_short_readme(name, specialization))
